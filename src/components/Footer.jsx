ed4a9dfee8b0715f5ebddaff222cf2d8bb52fe64
Yamil Castillo <yamilcs@gmail.com>
2023-07-09 22:43:27 -0400

    subiendo tarea-9

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer>
+        <p>Derechos reservados para este Footer, Yamil Castillo &copy; 2023</p>
+      </footer>   
+      );
+}
+ 
+export default Footer;
+<footer>
+        <p>Derechos reservados para este Footer, Yamil Castillo &copy; 2023</p>
+      </footer>   
